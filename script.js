4fa3cba608e688cbc4f73e43d80ab0926cf2297a
Shahed Khan <mdshahedulalamk@gmail.com>
2025-08-29 20:25:27 +0600

    Call and call history feature

--- script.js
+++ script.js
@@ -6,3 +6,42 @@ const allService = document.getElementById("all-service").addEventListener("clic
     heartCount.textContent = totalCount + 1;
   }
 });
+
+
+// Call section and call history feature 
+
+const coin = document.getElementById("coin");
+const callBtn = document.querySelectorAll(".call-btn");
+const cardTitle = document.querySelectorAll(".card-title");
+const callNumber = document.querySelectorAll(".call_number");
+const history = document.getElementById("history");
+
+const clearHistory = document.getElementById("clearHistory");
+
+
+
+callBtn.forEach((call, i) => {
+    call.addEventListener('click', () => {
+        if (coin.innerText <20){
+            alert("You have insufficient balance")
+            return
+        }
+        alert("📞 Calling " + cardTitle[i].innerText + " " + callNumber[i].innerText);
+        history.innerHTML +=`
+                <div class="flex items-center justify-between bg-gray-100 p-3 rounded-[8px] mb-3 mx-3">
+                    <div>
+                        <h1 class="font-bold text-[16px]">${cardTitle[i].innerText}</h1>
+                        <p class="text-[16px] text-gray-500">${callNumber[i].innerText}</p>
+                    </div>
+                    <p class="text-gray-800">${new Date().toLocaleTimeString()}</p>
+                </div>`
+        coin.innerText = coin.innerText - 20;
+        return coin
+        
+    });
+});
+
+
+clearHistory.addEventListener('click', () => {
+    history.innerHTML = '';
+});
